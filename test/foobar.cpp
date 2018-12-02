c65ce40ddfacb52bf0cddaf878126715d5b2b51e
Odin Phillips <odin.phillips@gmail.com>
2018-12-02 11:55:45 +0000

    Add googletest library and some simple tests

--- test/foobar.cpp
+++ test/foobar.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, foobar test!" << std::endl;
-    return 0;
+#include "gtest/gtest.h"
+
+static int add(int a, int b) {
+    return a + b;
+}
+
+TEST(AddTest, HandlesZeroInput) {
+    EXPECT_EQ(add(0, 0), 0);
+}
+
+// Example failing test
+TEST(AddTest, FakeFail) {
+    EXPECT_EQ(add(1, 1), 3);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
