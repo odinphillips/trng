baf164c50e8cd85fceade0dde0f6d334037b7268
Odin Phillips <odin.phillips@gmail.com>
2020-01-11 15:21:44 +0000

    Added scratch folder with portaudio play sound demo - not working on tablet though?!

---
+++ scratch/portaudio_demo/x.ycm_extra_conf.py
@@ -0,0 +1,5 @@
+def Settings( **kwargs ):
+    return {
+        'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-I', '/data/data/com.termux/files/home/git/trng/3rdParty/portaudio/include' ]
+    }
+
